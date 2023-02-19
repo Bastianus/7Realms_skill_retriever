1c089f4df51fd19fb0a870cdc2a8e7cc744c90d3
Bastianus <notgoingtohappen@live.nl>
2023-02-19 21:49:34 +0100

    results in console

---
+++ 7Realms_skill_retriever/Data/VaardigheidMetAantal.cs
@@ -0,0 +1,16 @@
+﻿namespace _7Realms_skill_retriever.Data
+{
+    internal class VaardigheidMetAantal : Vaardigheid
+    {
+        public int Aantal { get; private set; }
+        internal VaardigheidMetAantal(string naam, int niveau, int aantal) : base(naam, niveau) 
+        {
+            Aantal = aantal;
+        }
+
+        public void HoogAantalOp() 
+        {
+            Aantal++;
+        }
+    }
+}
--- 7Realms_skill_retriever/Tekst.cs
+++ 7Realms_skill_retriever/Tekst.cs
@@ -1,13 +1,63 @@
-﻿namespace _7Realms_skill_retriever
+﻿using _7Realms_skill_retriever.Data;
+using _7Realms_skill_retriever.Excel;
+
+namespace _7Realms_skill_retriever
 {
     internal static class Tekst
     {
-        internal static void DisplayStartInstructions(string inputPath)
+        internal static void DisplayStartInstructions(string inputDirectory)
         {
             Console.WriteLine("Plaats alle excel karakter formulieren die je wilt analyseren in de input folder van dit programma.");
-            Console.WriteLine($"Deze is te vinden in: {inputPath}");
+            Console.WriteLine();
+            Console.WriteLine($"Deze is te vinden in: {inputDirectory}");
+            Console.WriteLine();
+            Console.WriteLine("Zorg dat je zelf geen van deze excel bestanden open hebt.");
+            Console.WriteLine();
             Console.WriteLine("Druk daarna op een toets (met deze window geselecteerd) om verder te gaan.");
-            Console.ReadLine();
+            Console.ReadKey();
+        }
+
+        internal static void DisplayResultaten(List<ExcelGegevens> gegevens)
+        {
+            Console.WriteLine("De volgende spelers met hun karakters waren geselecteerd:");
+
+            var vaardigheden = new List<Vaardigheid>();
+
+            foreach (var karakter in gegevens)
+            {
+                Console.WriteLine($"Speler naam: {karakter.SpelerNaam}. Karakter naam: {karakter.KarakterNaam}");
+
+                vaardigheden.AddRange(karakter.Vaardigheden);
+            }
+            var uniekeVaardigheden = new List<VaardigheidMetAantal>();
+
+            vaardigheden.ForEach(vaardigheid =>
+            {
+                if (!uniekeVaardigheden.Any(x => x.Naam == vaardigheid.Naam && x.Niveau == vaardigheid.Niveau))
+                {
+                    uniekeVaardigheden.Add(new VaardigheidMetAantal(vaardigheid.Naam, vaardigheid.Niveau, 1));
+                }
+                else
+                {
+                    uniekeVaardigheden.Single(x => x.Naam == vaardigheid.Naam && x.Niveau == vaardigheid.Niveau).HoogAantalOp();
+                }
+            });
+
+            var geordend = uniekeVaardigheden.OrderBy(x => x.Naam).ThenBy(x => x.Niveau).ToList();
+
+            Console.WriteLine();
+            Console.WriteLine("Deze vaardigheden zijn gevonden:");
+            geordend.ForEach(v =>
+            {
+                Console.WriteLine($"{v.Naam}, niveau: {v.Niveau}, aantal:{v.Aantal}.");
+            });
+        }
+
+        internal static void Afsluiting()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Druk op een toets om af te sluiten.");
+            Console.ReadKey();
         }
     }
 }

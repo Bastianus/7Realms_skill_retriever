b259c80f709bd0dc429e8070f8771de8021e5b06
Bastianus <notgoingtohappen@live.nl>
2023-02-21 19:02:59 +0100

    Ambacht check en tekst output toegevoegd

--- 7Realms_skill_retriever/Tekst.cs
+++ 7Realms_skill_retriever/Tekst.cs
@@ -22,44 +22,61 @@ namespace _7Realms_skill_retriever
         {
             Console.Clear();
 
+            DisplaySpelersAndKarakters(gegevens);
+
+            DisplayVaardigheden(gegevens);
+
+            DisplayAmbachten(gegevens);
+        }
+
+        internal static void Afsluiting()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Druk op een toets om af te sluiten.");
+            Console.ReadKey();
+        }
+
+        private static void DisplaySpelersAndKarakters(List<ExcelGegevens> gegevens)
+        {
             Console.WriteLine("De volgende spelers met hun karakters waren geselecteerd:");
 
             Console.WriteLine();
 
+            foreach (var karakter in gegevens)
+            {
+                Console.WriteLine($"Speler naam: {karakter.SpelerNaam}. Karakter naam: {karakter.KarakterNaam}");
+            }
+        }
+
+        private static void DisplayVaardigheden(List<ExcelGegevens> gegevens)
+        {
             var uniekeVaardigheden = new List<VaardigheidMetAantal>();
 
             foreach (var karakter in gegevens)
             {
-                Console.WriteLine($"Speler naam: {karakter.SpelerNaam}. Karakter naam: {karakter.KarakterNaam}");
-
-                VoegVaardighedenToe();
-
-                void VoegVaardighedenToe()
+                karakter.Vaardigheden.ForEach(kv =>
                 {
-                    karakter.Vaardigheden.ForEach(kv =>
+                    if (!uniekeVaardigheden.Any(uv => uv.Naam == kv.Naam && uv.Niveau == kv.Niveau))
                     {
-                        if (!uniekeVaardigheden.Any(uv => uv.Naam == kv.Naam && uv.Niveau == kv.Niveau))
-                        {
-                            uniekeVaardigheden.Add(new VaardigheidMetAantal(kv.Naam, kv.Niveau, 1, karakter.KarakterNaam));
-                        }
-                        else
-                        {
-                            uniekeVaardigheden.Single(x => x.Naam == kv.Naam && x.Niveau == kv.Niveau)
-                                .HoogAantalOp()
-                                .VoegKarakterToe(karakter.KarakterNaam);
-                        }
-                    });
-                }
+                        uniekeVaardigheden.Add(new VaardigheidMetAantal(kv.Naam, kv.Niveau, 1, karakter.KarakterNaam));
+                    }
+                    else
+                    {
+                        uniekeVaardigheden.Single(x => x.Naam == kv.Naam && x.Niveau == kv.Niveau)
+                            .HoogAantalOp()
+                            .VoegKarakterToe(karakter.KarakterNaam);
+                    }
+                });
+
             }
-           
-            var geordend = uniekeVaardigheden.OrderBy(x => x.Naam).ThenBy(x => x.Niveau).ToList();
 
             Console.WriteLine();
             Console.WriteLine("Deze vaardigheden zijn gevonden:");
 
+            var geordend = uniekeVaardigheden.OrderBy(x => x.Naam).ThenBy(x => x.Niveau).ToList();
+
             int longestNameLength = 0;
             geordend.Select(g => g.Naam).ToList().ForEach(n => longestNameLength = n.Length > longestNameLength ? n.Length : longestNameLength);
-
 
             geordend.ForEach(v =>
             {
@@ -70,11 +87,37 @@ namespace _7Realms_skill_retriever
             });
         }
 
-        internal static void Afsluiting()
+        private static void DisplayAmbachten(List<ExcelGegevens> gegevens)
         {
+            var uniekeAmbachten = new List<Ambacht>();
+
+            foreach (var karakter in gegevens)
+            {
+                if(!uniekeAmbachten.Any(a => a.Naam == karakter.Ambacht))
+                {
+                    uniekeAmbachten.Add(new Ambacht(karakter.Ambacht, karakter.KarakterNaam));
+                }
+                else
+                {
+                    uniekeAmbachten.Single(a => a.Naam == karakter.Ambacht).VerhoogAantal().VoegKarakterToe(karakter.KarakterNaam);
+                }
+            }
+
+            var geordend = uniekeAmbachten.OrderBy(a => a.Naam).ToList();
+
+            int longestNameLength = 0;
+            geordend.Select(g => g.Naam).ToList().ForEach(n => longestNameLength = n.Length > longestNameLength ? n.Length : longestNameLength);
+
             Console.WriteLine();
-            Console.WriteLine("Druk op een toets om af te sluiten.");
-            Console.ReadKey();
+            Console.WriteLine("De volgende ambachten zijn gevonden:");
+
+            geordend.ForEach(v =>
+            {
+                var karakters = new StringBuilder();
+                karakters.AppendJoin(", ", v.Karakters.OrderBy(k => k));
+
+                Console.WriteLine($"{v.Naam} {new string(' ', longestNameLength - v.Naam.Length)}, aantal:{v.Aantal}. Spelers: {karakters}");
+            });
         }
     }
 }
